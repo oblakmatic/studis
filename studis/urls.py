7ee5339010755ed90888ad5a58fef83c847d094c
Matic <mo2625@student.uni-lj.si>
2018-04-09 15:44:47 +0200

    sifranti UI

--- studis/urls.py
+++ studis/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, re_path, include
 from django.views.generic.base import TemplateView
 from django.contrib.auth import views as auth_views
 from studis.views import login, auth_view, logout, invalid
+
 urlpatterns = [
 	path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
