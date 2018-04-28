2c00f1e367ad3d07374cb494526ca6f71bf6e5a1
Aljaž <ar1961@student.uni-lj.si>
2018-04-28 13:01:46 +0200

    commit scriptWin

--- scriptWin.sh
+++ scriptWin.sh
@@ -3,6 +3,7 @@ find . -path "*/migrations/*.pyc"  -delete
 
 rm db.sqlite3
 
+
 python manage.py makemigrations
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminadmin')" | python manage.py shell
