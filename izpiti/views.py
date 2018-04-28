a9d57ea2db03501bed4289eaa8be5316d90af5b0
Aljaž <ar1961@student.uni-lj.si>
2018-04-28 20:39:26 +0200

    Razpis roka,prijava na izpit(zacetek)
    Razpis roka bo še za preverit in dodelat pred predstavmo.
    Prijava na izpit narejen zacetek.

--- izpiti/views.py
+++ izpiti/views.py
@@ -10,17 +10,49 @@ from django.core.exceptions import ValidationError
 
 # Create your views here.
 def index_izpiti(request):
-    #Prevert se treba kdo je user. Za referentko ni panike.
-    #nedela migrations
 
-
-    all_izvedbaPredmeta = IzvedbaPredmeta.objects.select_related()
+    #pofixat treba se ce user ne obstaja! --> dam pol vrjetn neb smel sploh nc kazat!
     
-    context = {
-        'arr': all_izvedbaPredmeta,
-        'curr_date': strftime("%Y-%m-%dT%H:%M", gmtime())
+    if(request.user.groups.all()[0].name == "referent"):
+        all_izvedbaPredmeta = IzvedbaPredmeta.objects.select_related()
+    
+        context = {
+            'arr': all_izvedbaPredmeta,
+            'curr_date': strftime("%Y-%m-%dT%H:%M", gmtime())
+            }
 
-        }
+    elif(request.user.groups.all()[0].name == "professors"):
+        email_ = request.user.email
+
+        all_ucitelj = Ucitelj.objects.all()
+        
+        flag = False
+        for ucitelj in all_ucitelj:
+            if ucitelj.email == email_:
+                ucitelj_ = ucitelj
+                flag = True
+
+        if flag == False:
+            return HttpResponse("Prijavi se z uciteljom, ki je v bazi")
+        
+        all_izvedbaPredmeta = IzvedbaPredmeta.objects.select_related()
+        list = []
+        for izvedba_predmeta in all_izvedbaPredmeta:
+            if izvedba_predmeta.ucitelj_1 == ucitelj_:
+                list.append(izvedba_predmeta)
+            elif izvedba_predmeta.ucitelj_2 == ucitelj_:
+                list.append(izvedba_predmeta)
+            elif izvedba_predmeta.ucitelj_3 == ucitelj_:
+                list.append(izvedba_predmeta)
+         
+        
+        context = {
+            'arr': list,
+            'curr_date': strftime("%Y-%m-%dT%H:%M", gmtime())
+
+            }
+    else:
+        return HttpResponse("Nimaš dovoljenja.")
 
     return render(request,'index_izpiti.html',context)
 	
@@ -49,12 +81,36 @@ def dodaj_izpit(request):
 
 def prijava(request):
 
-    all_roki = Rok.objects.select_related()
-    print(all_roki)
+    if(request.user.groups.all()[0].name == "students"):
+        all_roki = Rok.objects.select_related()
+        email_stud = request.user.email
+
+       
+        for student in Student.objects.all():
+            if student.email == email_stud:
+                curr_student = student
+
+        if curr_student is None:
+            return HttpResponse("Student ne obstaja!")
+        else:
+            all_predmetiStudenta = PredmetiStudenta.objects.all()
+
+            #gres cez predmetiStudenta da pomachas vpis
+            for predmetiStudenta in all_predmetiStudenta:
+                if predmetiStudenta.vpis.student.email == curr_student.email:
+                    curr_predmetiStudenta = predmetiStudenta
+        
+        #print(curr_predmetiStudenta.vpis.student.ime)
+        #pazi ker ce gres gledat tko kt js pol je lahko izvedbaPredmeta za en predmet z istmu imeno za 2 leti!
+
+
+
+    else:
+        return HttpResponse("Nimaš dovoljenja.")
 
     context={
-        'arr': all_roki
-        }
+    'arr': all_roki
+    }
 
     return render(request,'prijava.html',context)
 
