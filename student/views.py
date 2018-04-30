e5958b2b631e9ca03129dbb16b0a11b982638807
Aljaž <ar1961@student.uni-lj.si>
2018-04-30 15:52:25 +0200

    Zacetek na izpis potril vpisa

--- student/views.py
+++ student/views.py
@@ -13,6 +13,13 @@ from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.pagesizes import letter, A4, landscape
 from reportlab.lib import colors 
 
+import time
+from reportlab.lib.enums import TA_JUSTIFY
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
+
 # Create your views here.
 def upload_file(request):
 
@@ -316,3 +323,100 @@ def export_pdf(request):
 
 	doc.build(elements)
 	return response
+
+def potrdi_studente(request):
+	if(request.user.groups.all()[0].name == "referent"):
+		if request.method == 'POST' and 'potrdi_studenta' in request.POST:
+
+			vpis_student_email = request.POST.get('vpis_email')
+			
+			for vpis in Vpis.objects.all():
+				if str(vpis.student.email) == vpis_student_email:
+					if vpis.potrjen == False:
+						vpis.potrjen = True
+						vpis.save()
+					else:
+						vpis.potrjen = False
+						vpis.save()
+
+		if request.method == 'POST' and 'natisni' in request.POST:
+
+			vpis_student_email = request.POST.get('vpis_email')
+			vpis_ = None
+			for vpis in Vpis.objects.all():
+				if str(vpis.student.email) == vpis_student_email:
+					vpis_ = vpis
+
+
+			response = HttpResponse(content_type='application/pdf')
+			response['Content-Disposition'] = 'attachment; filename="studenti.pdf"'
+			
+			doc = SimpleDocTemplate(response,pagesize=letter,
+						rightMargin=72,leftMargin=72,
+						topMargin=72,bottomMargin=18)
+			Story=[]
+			logo = "student/Logo_UL_FRI.png"
+			magName = "Pythonista"
+			issueNum = 12
+			subPrice = "99.00"
+			limitedDate = "03/05/2010"
+			freeGift = "tin foil hat"
+ 
+			formatted_time = time.ctime()
+			full_name = vpis_.student.ime + " " +  vpis_.student.priimek 
+			address_parts = ["411 State St.", "Marshalltown, IA 50158"]
+ 
+			im = Image(logo, 2*inch, 2*inch)
+			Story.append(im)
+ 
+			styles=getSampleStyleSheet()
+			styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+			ptext = '<font size=12>%s</font>' % formatted_time
+ 
+			Story.append(Paragraph(ptext, styles["Normal"]))
+			Story.append(Spacer(1, 12))
+ 
+			# Create return address
+			ptext = '<font size=12>%s</font>' % full_name
+			Story.append(Paragraph(ptext, styles["Normal"]))       
+			for part in address_parts:
+				ptext = '<font size=12>%s</font>' % part.strip()
+				Story.append(Paragraph(ptext, styles["Normal"]))   
+ 
+			Story.append(Spacer(1, 12))
+			ptext = '<font size=12>Dear %s:</font>' % full_name.split()[0].strip()
+			Story.append(Paragraph(ptext, styles["Normal"]))
+			Story.append(Spacer(1, 12))
+ 
+			ptext = '<font size=12>We would like to welcome you to our subscriber base for %s Magazine! \
+					You will receive %s issues at the excellent introductory price of $%s. Please respond by\
+					%s to start receiving your subscription and get the following free gift: %s.</font>' % (magName, 
+																											issueNum,
+																											subPrice,
+																											limitedDate,
+																											freeGift)
+			Story.append(Paragraph(ptext, styles["Justify"]))
+			Story.append(Spacer(1, 12))
+ 
+ 
+			ptext = '<font size=12>Thank you very much and we look forward to serving you.</font>'
+			Story.append(Paragraph(ptext, styles["Justify"]))
+			Story.append(Spacer(1, 12))
+			ptext = '<font size=12>Sincerely,</font>'
+			Story.append(Paragraph(ptext, styles["Normal"]))
+			Story.append(Spacer(1, 48))
+			ptext = '<font size=12>Ima Sucker</font>'
+			Story.append(Paragraph(ptext, styles["Normal"]))
+			Story.append(Spacer(1, 12))
+			doc.build(Story)
+			return response
+
+	else:
+		return HttpResponse("Nimaš dovoljenja.")
+
+	all_vpis = Vpis.objects.all()
+	context ={
+		'arr': all_vpis
+		}
+
+	return render(request, 'potrdi_studente.html',context)
