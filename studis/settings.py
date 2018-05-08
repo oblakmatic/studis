e884a82b58a794919ffb40e078c8f037eea206c0
Unknown <aljaz.verlic@gmail.com>
2018-05-08 23:17:50 +0200

    pdf test

--- studis/settings.py
+++ studis/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'wkhtmltopdf',
     'sifranti.apps.SifrantiConfig',
     'vpis.apps.VpisConfig',
     'student.apps.StudentConfig',
